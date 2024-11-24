a55960c978cc4375dfe0c0fc0b8706fbc2338f8f
Ibr707090 <ibrsupreme.604@gmail.com>
2024-11-24 14:09:37 +0530

    Update m.py

--- m.py
+++ m.py
@@ -316,7 +316,7 @@ def is_authorized(user_id):
 
 def run_action(user_id, message, ip, port, duration):
     # Generate random thread value
-    thread_value = random.randint(40, 140)
+    thread_value = random.randint(500, 800)
 
     # Log the action
     logging.info(f"User {user_id} started action on IP {ip}, Port {port}, Duration {duration}s")
